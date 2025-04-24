3b69734edbd2157463e7c6d1d6ba78d5ce71e704
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-04-24 13:18:10 -0500

    copia-segurida

--- Front/App/Includes/js/main.js
+++
@@ -1,6 +0,0 @@
-import { menuItems } from './menu/routerMenu.js';
-
-import { router } from './route/router.js';
-
-window.addEventListener('hashchange', router);
-window.addEventListener('load', router);
